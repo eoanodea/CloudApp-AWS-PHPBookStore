47b354ce4f34d37e2ce5c4749cfa5942902fa059
EC2 Default User <ec2-user@ip-172-31-51-170.ec2.internal>
2021-01-13 12:51:46 +0000

    Database connection for production

--- classes/Connection.php
+++ classes/Connection.php
@@ -1,10 +1,10 @@
 <?php
 class Connection {
 
-    private static $host = "localhost";
-    private static $database = "cloudAD_bookstore";
-    private static $username = "root";
-    private static $password = "";
+    private static $host = "bookstoresampledb.c1loew3e2yv5.us-east-1.rds.amazonaws.com";
+    private static $database = "bookstoresample_db";
+    private static $username = "admin";
+    private static $password = "secretBS";
 
     public static function getInstance() {
         $dsn = 'mysql:host=' . Connection::$host . ';dbname=' . Connection::$database;
